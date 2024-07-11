07e91635f7cfd07a7c2af0ea2c463166758b55b5
Douglas Obara <douglasobara97@gmail.com>
2024-07-11 16:50:25 +0300

    create boiler plate

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,12 @@ import ProductList from "@/components/ProductList";
 import { getData } from "@/lib/getData";
 import Link from "next/link";
 const HomePage = async () => {
+  // Fetching all products from an api
   const products = (await getData("https://fakestoreapi.com/products")) ?? [];
 
   return (
     <div className=" px-20 py-16">
-      <h2 className="text-4xl font-bold p-4">Redux Toolkit</h2>
+      <h2 className="text-4xl font-bold p-4">🛒 Douglas254 Commerce</h2>
       <div className="bg-white border border-gray-300 rounded-lg  dark:bg-gray-700 dark:border-gray-700 text-slate-800 overflow-hidden">
         <div className="bg-slate-100 dark:bg-gray-800 py-3 px-6 font-semibold border-b border-gray-300 dark:border-gray-600 text-slate-800 dark:text-slate-100 flex justify-between items-center">
           <h2>Products</h2>
